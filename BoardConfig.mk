7f5da9dcff045b6f594dc53a86911aba0c91be63
rothnic <nlr06886@gmail.com>
2011-02-06 02:11:56 -0600

    Merged in Adam board config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -2,12 +2,14 @@ USE_CAMERA_STUB := true
 BOARD_USE_FROYO_LIBCAMERA := true
 
 # inherit from the proprietary version
--include vendor/nvidia/harmony/BoardConfigVendor.mk
+-include vendor/notionink/adam/BoardConfigVendor.mk
 
+TARGET_NO_BOOTLOADER := true
 TARGET_BOARD_PLATFORM := tegra
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a
+TARGET_BOOTLOADER_BOARD_NAME := adam
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_ARCH_VARIANT_FPU := vfpv3-d16
 TARGET_CPU_SMP := true
@@ -17,8 +19,7 @@ TARGET_USES_OLD_LIBSENSORS_HAL := true
 # custom recovery ui
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/nvidia/harmony/recovery/recovery_ui.c
 
-TARGET_NO_BOOTLOADER := true
-TARGET_BOOTLOADER_BOARD_NAME := harmony
+TARGET_BOOTLOADER_BOARD_NAME := adam
 
 # Modem
 TARGET_NO_RADIOIMAGE := true
@@ -34,7 +35,7 @@ WIFI_DRIVER_MODULE_NAME     := "dhd"
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_CSR := true
 
-BOARD_KERNEL_CMDLINE := mem=448M@0M nvmem=64M@448M vmalloc=192M video=tegrafb console=ttyS0,115200n8 usbcore.old_scheme_first=1 cpuid=200102 devicetype=1002 btmac=9c5ed6131a00 tegraboot=nand mtdparts=tegra_nand:16384K@12032K(misc),16384K@62208K(recovery),16384K@79104K(boot),204800K@96000K(system),222464K@301312K(cache),4096K@7424K(bootbmp),32768K@28928K(logodata) androidboot.hardware=harmony
+BOARD_KERNEL_CMDLINE := 
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_PAGE_SIZE := 0x00000800
 
@@ -52,11 +53,9 @@ BOARD_EGL_CFG := device/nvidia/harmony/egl.cfg
 # mtd1: 01000000 00020000 "recovery"
 # mtd2: 01000000 00020000 "boot"
 # mtd3: 0c800000 00020000 "system"
-# mtd4: 0d940000 00020000 "cache"
-# mtd5: 00400000 00020000 "bootbmp"
-# mtd6: 02000000 00020000 "logodata"
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01000000
+# mtd4: 0fe20000 00020000 "cache"
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x10000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x10000000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x0c800000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
 BOARD_FLASH_BLOCK_SIZE := 131072
@@ -67,7 +66,7 @@ BOARD_HAS_NO_MISC_PARTITION := true
 # Indicate that the board has an Internal SD Card
 BOARD_HAS_SDCARD_INTERNAL := true
 
-TARGET_PREBUILT_KERNEL := device/nvidia/harmony/kernel
+TARGET_PREBUILT_KERNEL := device/notionink/adam/kernel
 
 # Below is a sample of how you can tweak the mount points using the board config.
 # This is for the Samsung Galaxy S.
@@ -77,10 +76,10 @@ BOARD_DATA_DEVICE := /dev/block/mmcblk3p2
 BOARD_DATA_FILESYSTEM := ext3
 BOARD_CACHE_DEVICE := /dev/block/mtdblock4
 BOARD_CACHE_FILESYSTEM := yaffs2
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk2p1
-BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk3p1
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk3p1 	
+BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk2p1
 BOARD_SDCARD_DEVICE_INTERNAL := /dev/block/mmcblk3p1
-BOARD_SDEXT_DEVICE := /dev/block/mmcblk2p2
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk2p1
 #BOARD_HAS_DATADATA := true
 #BOARD_DATADATA_DEVICE := /dev/block/stl10
 #BOARD_DATADATA_FILESYSTEM := auto
